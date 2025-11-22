2777750058ff8b7448995879df5dfea8e27ea063
Carlos Yáñez <carlosyanez997@gmail.com>
2025-11-22 15:32:15 +0100

    Add solution for exercise 1 b2

--- 1b/ej1b2.py
+++ 1b/ej1b2.py
@@ -66,9 +66,10 @@ Exemple:
 import math
 
 def calculate_angle(angle):
-    # Write here your code
-    pass
+    radians = math.radians(angle)
+    sin = math.sin(radians)
+    return round(sin, 2)
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# print(calculate_angle(270))
+print(calculate_angle(270))
