684337338293719be9ff18e74e61845d096d3ee1
Carlos Yáñez <carlosyanez997@gmail.com>
2025-11-22 15:43:41 +0100

    Add solution for exercise 1 b3

--- 1b/ej1b3.py
+++ 1b/ej1b3.py
@@ -51,9 +51,11 @@ import matplotlib.pyplot as plt
 # Esta función deberá configurar la gráfica en la variable plt
 # Aquesta funció haurà de configurar la gràfica en la variable plt
 def line_graph(x, y):
-    # Write here your code
-    pass
-
+    plt.plot(x, y)            
+    plt.title("Graph")
+    plt.xlabel("Axis X")
+    plt.ylabel("Axis Y")
+    plt.grid(True)
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 # line_graph([1, 2, 3, 4, 5], [2, 4, 6, 8, 10])
